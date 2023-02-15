76ef528b6127497d53cc3a0c87f669487d004453
SimonH <huetsimon35@gmail.com>
2023-02-15 23:02:51 +0100

    feat: Adding Redux,RTK, init store and clean CRA Public folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import App from '@Components/App';
+import {createStore} from '@Utils/Store';
+
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
