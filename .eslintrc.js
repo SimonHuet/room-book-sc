c7b1d49600958511d064ab6c792c2b5c6a074dd6
SimonH <huetsimon35@gmail.com>
2023-02-18 12:25:57 +0100

    fix: Eslint and Tsconfig config errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   rules: {
     'linebreak-style': 'off',
