694e9552c8c4fbab711550f924ed7e9968246928
SimonH <huetsimon35@gmail.com>
2023-02-19 18:19:13 +0100

    fix: Sorting bookings and fix current event dispaly

---
+++ src/Utils/State.ts
@@ -0,0 +1,16 @@
+import { Comparer } from '@reduxjs/toolkit'
+import compareAsc from 'date-fns/compareAsc'
+import compareDesc from 'date-fns/compareDesc'
+
+type SortDirection = 'asc' | 'desc'
+
+export const dateSorter: (
+  key: string,
+  direction?: SortDirection
+) => Comparer<any> =
+  (key, direction = 'asc') =>
+  (a, b) =>
+    ({
+      asc: compareAsc(new Date(a[key]), new Date(b[key])),
+      desc: compareDesc(new Date(a[key]), new Date(b[key])),
+    }[direction])
